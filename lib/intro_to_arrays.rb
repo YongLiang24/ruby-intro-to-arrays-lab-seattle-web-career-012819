a3d2dac2933e3740b90a6098137671e519491402
yong liang <yongliang24@gmail.com>
2019-01-19 23:04:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,3 +7,18 @@ def array_with_two_elements
   array_with_two_elements = ["a", "b"]
   @my_two_array = array_with_two_elements
 end
+
+def first_element(str)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  str = @taylor_swift.first
+end
+
+def third_element(str)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  str = @taylor_swift[2]
+end
+
+ def last_element(str)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  str = @taylor_swift[-1]
+end
